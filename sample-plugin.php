d7a02de09e7118b8694bc4dc14d6e64a20b06409
Nazmul Ahsan <n.mukto@gmail.com>
2023-03-27 10:04:45 +0600

    init

---
+++ sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Sample Plugin
+ * Plugin URI: https://github.com/pluggableio/sample-plugin
+ * Author: Pluggable, Inc
+ * Author URI: https://pluggable.io/
+ * Version: 0.1
+ * Text Domain: sample-plugin
+ * Domain Path: languages
+ */
